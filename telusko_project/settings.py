0038919b029551d868f7343cd422461419274eb3
akimwale <132432369+akimwale@users.noreply.github.com>
2024-08-24 04:40:09 +0100

    Update settings.py

--- telusko_project/settings.py
+++ telusko_project/settings.py
@@ -14,6 +14,12 @@ from pathlib import Path
 import os
 import environ
 
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
